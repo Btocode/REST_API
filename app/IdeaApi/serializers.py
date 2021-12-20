d95bd84726f5aaeb4ff7c54b7f8a9f9546a0e8c7
Afsan Saeed <afsansaeed2017@gmail.com>
2021-12-20 21:02:22 +0600

    frontend added

--- app/IdeaApi/serializers.py
+++ app/IdeaApi/serializers.py
@@ -2,11 +2,17 @@ from django.db import models
 from rest_framework import fields, serializers
 from .models import Suggestion, User,Idea
 
+
+# I have changed this section for testing purpose 19.12.21
 class userSerializer(serializers.ModelSerializer):
   class Meta():
     model = User
-    fields = ('firstName','lastName')
+    fields = ('firstName','lastName','email','password','age','gender')
 
+class LoginSerializer(serializers.ModelSerializer):
+  class Meta():
+    model = User
+    fields = ('email',"password")
 class suggSerializer(serializers.ModelSerializer):
   class Meta():
     model = Suggestion
--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^1&3l$3158mw&(#bm*$u$m%sh!#-31uss9vd^7qvoo5*tt_q-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
